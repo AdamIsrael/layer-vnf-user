549e4798caadde3523d6cdecd431834907340538
Adam Israel <adam.israel@canonical.com>
2019-04-12 11:09:48 -0400

    Initial import

---
+++ README.md
@@ -0,0 +1,26 @@
+# Overview
+
+This is an example of an Open Source Mano (OSM) charm used with the Network Service (ns) example. 
+
+Network Service charms are an **experimental** feature, expected to be released with OSM R6 in the spring of 2019.
+
+# Usage
+
+
+## Known Limitations and Issues
+
+This not only helps users but gives people a place to start if they want to help
+you add features to your charm.
+
+# Configuration
+
+None
+
+# Contact Information
+## Open Source Mano (OSM)
+
+  - [OSM website](https://osm.etsi.org/)
+  - [OSM bug tracker](https://osm.etsi.org/bugzilla/)
+  - [OSM_TECH](mailto:OSM_TECH@list.etsi.org) mailing list
+  - [Slack](https://join.slack.com/t/opensourcemano/shared_invite/enQtMzQ3MzYzNTQ0NDIyLWJkMzRjNDM0MjFjODYzMGQ3ODIzMzJlNTg2ZGI5OTdiZjFiNDMyMzYxMjRjNDU4N2FmNjRjNzY5NTE1MjgzOTQ)
+
---
+++ reactive/vnf_user.py
@@ -0,0 +1,49 @@
+from charmhelpers.core.hookenv import (
+    action_get,
+    action_fail,
+    action_set,
+    status_set,
+    log,
+)
+
+from charms.reactive import (
+    clear_flag,
+    set_flag,
+    when,
+    when_not,
+)
+import random
+
+
+@when_not('vnf-user.installed')
+def install_vnf_user():
+    set_flag('vnf-a.installed')
+
+
+@when('actions.add-user')
+def action_add_user():
+    """Run the touch command.
+
+    Runs touch on vnf-a and vnf-b and returns the success or failure for each.
+    """
+    err = ''
+    user_id = 0
+    try:
+        username = action_get('username')
+        tariff = action_get('tariff')
+
+        # If this were a functional VNF, it would add the username to its
+        # database. For the purposes of demonstrating actions, this will return
+        # a random number.
+        user_id = random.randint(1, 100)
+
+    except Exception as err:
+        # This marks the action as having failed, so the ns charm knows not to
+        # continue with the add_user operation.
+        action_fail(str(err))
+    else:
+        # This will put the user_id in the action output, which will be read
+        # by the ns charm. Multiple values could be returned here.
+        action_set({'user-id': user_id})
+    finally:
+        clear_flag('actions.add-user')
